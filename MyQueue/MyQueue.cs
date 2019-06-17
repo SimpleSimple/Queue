0961ee9015417a3ef583bfc3415a602a062f716a
playitouch@163.com <playitouch@163.com>
2019-06-17 11:09:09 +0800

    第一次添加

---
+++ MyQueue/MyQueue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    public class MyQueue<T>
+    {
+        T[] data;
+
+        public void enqueue() { }
+
+        public void dequeue() { }
+
+        public void getFront() { }
+
+        public void size() { }
+
+        public void isEmpty() { }
+    }
+}
